923273909e319c54072a0e0e13dd9ee016174497
Kevin Zhou <kevinzhou22@outlook.com>
2020-12-15 12:42:36 +1100

    Set up functions for adding, modifying, and removing Book objects in the library

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Mechanisim for removing focus outline without disturbing accessibility */
 document.body.addEventListener("mousedown", () => document.body.classList.add("using-mouse"));
 document.body.addEventListener("keydown", (e) => {
     if(e.key === "Tab") {
@@ -5,6 +6,19 @@ document.body.addEventListener("keydown", (e) => {
     }
 });
 
+
+
+
+
+/* Initilialization for various controls on a page */
+const newBookButton = document.querySelector(".new-book");
+newBookButton.addEventListener("click", () => {
+    document.querySelector(".bg-modal").style.display =  "flex";
+});
+document.querySelector(".close").addEventListener("click", closeModal);
+document.querySelector(".cancel").addEventListener("click", closeModal);
+
+/* Defines what a Book object is and how they are stored */
 
 let myLibrary = [];
 
@@ -19,20 +33,33 @@ Book.prototype.info = function() {
     return `${title} by ${author}, ${page} pages, ${read ? "already read" : "not read yet"}`;
 };
 
-function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
+/* Handles adding a new book to the library */
+function addBookToLibrary(title,author,pages,read) {
+    const newBook = new Book(title,author,pages,read);
+    myLibrary.push(newBook);
+    rerender();
 }
 
-const newBookButton = document.querySelector(".new-book");
-newBookButton.addEventListener("click", () => {
-    document.querySelector(".bg-modal").style.display =  "flex";
-});
+/* Handles deleting a book to the library */
+function deleteBookFromLibrary(book) {
+    myLibrary.splice(myLibrary.indexOf(newBook,1))
 
-document.querySelector(".close").addEventListener("click", closeModal);
+}
 
-document.querySelector(".cancel").addEventListener("click", closeModal);
+/* Handles updating the information associated with a book in the library */
+function updateBookInLibrary(book,title,author,pages,read) {
+    book.title = title;
+    book.author = author;
+    book.pages = pages;
+    book.read=read;
+}
 
+/* updates the graphical display of the library to comply with the myLibrary array, 
+displaying the last element in the array first on the graphical interface */
+function rerender() {
 
+}
+/* callback function for closing the modal that enables input of data for a new book */
 function closeModal() {
     document.querySelector(".bg-modal").style.display = "none";
     document.querySelector(".bg-modal form").reset();
